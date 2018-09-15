b34b84c3cbf6baace9d8dcec54274a24a6e5f5d8
Bettina <burri.bettina@gmail.com>
2018-09-15 23:58:13 +0200

    Image of Outlook added to repro

--- frontend/js/data_editor.js
+++ frontend/js/data_editor.js
@@ -1,14 +1,1 @@
-(function() {
-    var local_data = "json/sample.json";
-    $.getJSON( local_data, {
-      format: "json"
-    })
-      .done(function( data ) {
-        $.each( data.classifiedCompilation, function( i, item ) {
-          $( "<img>" ).attr( "src", item.media.m ).appendTo( "#images" );
-          if ( i === 3 ) {
-            return false;
-          }
-        });
-      });
-  })();
+var obj = JSON.parse(json/sampe.json);
